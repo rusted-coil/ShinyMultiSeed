0416c68d84cf4581fd649fd94f31a704ccf6c70a
rusted-coil <rusted.coil@gmail.com>
2024-08-27 13:43:27 +0900

    MainForm整理

---
+++ ShinyMultiSeed/Main/MainFormPresenter.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive.Disposables;
+using System.Runtime.Versioning;
+
+namespace ShinyMultiSeed.Main
+{
+    [SupportedOSPlatform("win-x64")]
+    internal sealed class MainFormPresenter : IDisposable
+    {
+        readonly MainForm m_MainForm;
+        readonly CompositeDisposable m_Disposables = new CompositeDisposable();
+
+        public MainFormPresenter()
+        { 
+            m_MainForm = new MainForm();
+        }
+
+        public void Run()
+        {
+            Application.Run(m_MainForm);
+        }
+
+        public void Dispose()
+        {
+            m_Disposables.Dispose();
+        }
+    }
+}
--- ShinyMultiSeed/Program.cs
+++ ShinyMultiSeed/Program.cs
@@ -1,17 +1,16 @@
+using ShinyMultiSeed.Main;
+
 namespace ShinyMultiSeed
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            using (var presenter = new MainFormPresenter())
+            {
+                presenter.Run();
+            }
         }
     }
 }
