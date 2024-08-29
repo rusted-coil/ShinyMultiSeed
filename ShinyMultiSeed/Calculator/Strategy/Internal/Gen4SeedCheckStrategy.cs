a01f9a63b238543f994878da9b0b7ab898dad954
rusted-coil <rusted.coil@gmail.com>
2024-08-29 20:27:58 +0900

    Strategy修正

--- ShinyMultiSeed/Calculator/Strategy/Internal/Gen4SeedCheckStrategy.cs
+++ ShinyMultiSeed/Calculator/Strategy/Internal/Gen4SeedCheckStrategy.cs
@@ -1,15 +1,13 @@
 ﻿using Gen4RngLib.Rng;
+using System.Diagnostics;
 
 namespace ShinyMultiSeed.Calculator.Strategy.Internal
 {
     internal sealed class Gen4SeedCheckStrategy : ISeedCheckStrategy<uint, IGen4SeedCheckResult>
     {
-        // 高速化のため、スレッドごとにRNGオブジェクトを使い回す
-        readonly ThreadLocal<ILcgRng> m_MainRng;
-        readonly ThreadLocal<ILcgRng> m_TempRng;
-        readonly ThreadLocal<ILcgRng> m_ReverseRng;
-
-        readonly ThreadLocal<int[]> m_NatureFlags;
+        // 高速化のため、スレッドごとにオブジェクトを使い回す
+        readonly ThreadLocal<ILcgRng> m_Rng;
+        readonly ThreadLocal<(int Flag, int Synchro)[]> m_NatureFlags;
 
         class Result : IGen4SeedCheckResult
         {
@@ -34,7 +32,7 @@ namespace ShinyMultiSeed.Calculator.Strategy.Internal
         readonly uint m_SpdIVMax;
         readonly bool m_UsesSynchro;
 
-        public Gen4SeedCheckStrategy(Gen4SeedCheckStrategyArgs args, Func<ILcgRng> createLcgRng, Func<ILcgRng> createReverseLcgRng)
+        public Gen4SeedCheckStrategy(Gen4SeedCheckStrategyArgs args, Func<ILcgRng> createLcgRng)
         {
             m_isHgss = args.IsHgss;
             m_EncountOffset = args.EncountOffset;
@@ -51,18 +49,17 @@ namespace ShinyMultiSeed.Calculator.Strategy.Internal
             m_SpdIVMax = args.SpdIVMax;
             m_UsesSynchro = args.UsesSynchro;
 
-            m_MainRng = new ThreadLocal<ILcgRng>(createLcgRng);
-            m_TempRng = new ThreadLocal<ILcgRng>(createLcgRng);
-            m_ReverseRng = new ThreadLocal<ILcgRng>(createReverseLcgRng);
-            m_NatureFlags = new ThreadLocal<int[]>(() => new int[50]);
+            m_Rng = new ThreadLocal<ILcgRng>(createLcgRng);
+            m_NatureFlags = new ThreadLocal<(int Flag, int Synchro)[]>(() => new (int Flag, int Synchro)[50]);
         }
 
         public IGen4SeedCheckResult Check(uint initialSeed)
         {
-            var mainRng = m_MainRng.Value;
-            var tempRng = m_TempRng.Value;
-            var reverseRng = m_ReverseRng.Value;
-            mainRng.Seed = initialSeed;
+            var rng = m_Rng.Value;
+            var natureFlags = m_NatureFlags.Value;
+            Debug.Assert(rng != null);
+            Debug.Assert(natureFlags != null);
+			rng.Seed = initialSeed;
 
             bool isOk = false;
             uint startPosition = 0;
@@ -74,30 +71,31 @@ namespace ShinyMultiSeed.Calculator.Strategy.Internal
             // TODO 目当てのポジションまで消費するのは高速化したい
             for (int i = 0; i < m_PositionMin + m_EncountOffset; ++i)
             {
-                mainRng.Next();
+				rng.Next();
             }
 
-            uint pidLower = mainRng.Next(); // r[PositionMin + EncountOffset]
-            uint pidUpper = mainRng.Next();
-            uint ivs1 = mainRng.Next();
+            uint pidLower = rng.Next(); // r[PositionMin + EncountOffset]
+            uint pidUpper = rng.Next();
+            uint ivs1 = rng.Next();
             uint ivs2;
 
             for (int i = 0; i < 50; ++i)
             {
-                m_NatureFlags.Value[i] = -1;
+				natureFlags[i].Flag = -1;
             }
 
             for (uint i = m_PositionMin + m_EncountOffset; i <= m_PositionMax + m_EncountOffset; ++i, pidLower = pidUpper, pidUpper = ivs1, ivs1 = ivs2)
             { 
-                ivs2 = mainRng.Next();
+                ivs2 = rng.Next();
 
                 // この時点でのpidとivsはr[i]から生成されるもの
 
                 uint nature = (pidUpper << 16 | pidLower) % 25;
 
-                if (m_NatureFlags.Value[nature * 2 + i % 2] >= 0) // 生成できる性格だった
+                if (!m_DeterminesNature || natureFlags[nature + (i % 2) * 25].Flag >= 0) // 生成できる性格だった
                 {
                     bool isPass = true;
+
                     // 色違い判定
                     if (m_isShiny && m_Tsv != ((pidLower ^ pidUpper) & 0xfff8))
                     {
@@ -123,30 +121,43 @@ namespace ShinyMultiSeed.Calculator.Strategy.Internal
                     {
                         // 適合個体だったらNatureFlagにかかれている消費数を出力して終了
                         isOk = true;
-                        startPosition = (uint)m_NatureFlags.Value[nature * 2 + i % 2];
-                        break;
+                        startPosition = (uint)natureFlags[nature + (i % 2) * 25].Flag;
+                        synchroNature = natureFlags[nature + (i % 2) * 25].Synchro;
+						break;
                     }
                     else
                     {
-                        // 適合していなかったらこの個体の性格フラグを✕に
-                        m_NatureFlags.Value[nature * 2 + i % 2] = -1;
+						// 適合していなかったらこの個体の性格フラグを✕に
+						natureFlags[nature + (i % 2) * 25].Flag = -1;
                     }
                 }
 
                 // フラグ更新
-                if (pidLower % 2 == 0) // シンクロに成功したら全てのフラグを◯に
+                if (m_DeterminesNature)
                 {
-                    for (int n = 0; n < 25; ++n)
-                    {
-                        m_NatureFlags.Value[n * 2 + (1 - i % 2)] = (int)i;
-                    }
-                }
-                else
-                {
-                    // 通常の性格ロールフラグだけを◯に
-                    m_NatureFlags.Value[(pidLower % 25) * 2 + (1 - i % 2)] = (int)i;
-                }
-            }
+					if (m_UsesSynchro &&
+                        ((m_isHgss && pidLower % 2 == 0) || (!m_isHgss && (pidLower & 0x8000) == 0))) // シンクロに成功したら全てのフラグを◯に（上書きはしない）
+					{
+						uint offset = (1 - i % 2) * 25;
+						for (int n = 0; n < 25; ++n)
+						{
+							if (natureFlags[n + offset].Flag < 0)
+							{
+								natureFlags[n + offset] = ((int)i, n);
+							}
+						}
+					}
+					else
+					{
+                        // 通常の性格ロールフラグだけを◯に（上書きはしない）
+                        uint n = (m_isHgss ? pidLower % 25 : pidLower / 0xa3e);                        
+						if (natureFlags[n + (1 - i % 2) * 25].Flag < 0)
+						{
+							natureFlags[n + (1 - i % 2) * 25] = ((int)i, -1);
+						}
+					}
+				}
+			}
 
             return new Result { IsPassed = isOk, StartPosition = startPosition, SynchroNature = synchroNature };
         }
